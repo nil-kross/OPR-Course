22f6804f7b29afd383af1180bfc60b648e1f73ff
nil-kross <nil.kross@gmail.com>
2018-10-26 14:34:25 +0300

    Add GeneticAlgorithm & "Console GA" project

---
+++ Infrastructure/GeneticAlgorithm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lomtseu {
+    public class GeneticAlgorithm {
+        private Population population = null;
+
+        public GeneticAlgorithm(Population startingPopulation)
+        {
+            this.population = startingPopulation;
+        }
+    }
+}
