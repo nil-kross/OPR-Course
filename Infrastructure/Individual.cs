d933a5821c4175ae48e132000566526a4b8bc850
nil-kross <nil.kross@gmail.com>
2018-10-26 09:44:32 +0300

    Add Population class

---
+++ Infrastructure/Individual.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lomtseu {
+    public class Individual {
+        private IList<Chromosome> chromosomesList = null;
+
+        protected IEnumerable<Chromosome> Chromosomes {
+            get => this.chromosomesList;
+        }
+    }
+}
---
+++ Infrastructure/Population.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lomtseu {
+    public class Population {
+        private IList<Individual> individualsList = null;
+
+        public IEnumerable<Individual> Individuals {
+            get => this.individualsList;
+        }
+
+        public Int32 Size {
+            get => this.individualsList?.Count() ?? 0;
+        } 
+
+        public Population(IEnumerable<Individual> startPopulation = null) {
+            this.individualsList = new List<Individual>(startPopulation);
+        }
+    }
+}
