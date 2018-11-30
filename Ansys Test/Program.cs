e29b5d1654b9646004d12038498f719f23d57110
nil-kross <nil.kross@gmail.com>
2018-11-30 19:38:20 +0300

    Add ACT examples

---
+++ Ansys Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using Ansys.ACT.Interfaces.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ansys_Test {
+    class Program {
+        static void Main(string[] args) {
+            var context = new ProjectApplicationAPI;
+            Console.ReadKey();
+        }
+    }
+}
